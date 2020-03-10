5704175874566933d8dba0a940bddb40a62ad6b5
Monica Djunaidi <modjun12@gmail.com>
2020-03-10 13:37:43 -0500

    Add project files.

---
+++ SyntacticSugar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SyntacticSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var preds = new List<string>()
+            {
+                "Rinne",
+                "Jose",
+                "Saros"
+            };
+
+            var prey = new List<string>()
+            {
+                "Kane",
+                "Toews",
+                "Dach"
+            };
+
+            var dooDoo = new Bug("Poo", "Dangerous", preds, prey);
+            Console.WriteLine($"This is the name of the bug: {dooDoo.Name}");
+            Console.WriteLine($"THis is the species of the bug: {dooDoo.Species}");
+            Console.WriteLine($"This is the list of prey: {dooDoo.PreyList()}");
+            Console.WriteLine($"This is the list of predators: {dooDoo.PredatorList()}");
+
+            Console.ReadLine();
+        }
+    }
+}
